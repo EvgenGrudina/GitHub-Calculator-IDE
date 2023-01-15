aa05c7e5e06fde64e19964a8266a0c2a254a9e5d
yevhen <grudi4ek@gmail.com>
2023-01-15 22:54:02 +0200

    Move variables

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,19 +1,25 @@
 package app;
 
 public class Main {
+    private static int a;
+    private static int b;
+    private static int c;
+    private static int d;
+    private static int e;
+    private static int f;
 
     public static void main(String[] args) {
         System.out.println("Calculator for base calculations");
         System.out.println("Version 1.0");
-        int a = 8;
-        int b = 2;
-        int c = add(a, b);
+        a = 8;
+        b = 2;
+        c = add(a, b);
         System.out.println(c);
-        int d = multiply(a, b);
+        d = multiply(a, b);
         System.out.println(d);
-        int e = sub(a, b);
+        e = sub(a, b);
         System.out.println(e);
-        int f = div(a, b);
+        f = div(a, b);
         System.out.println(f);
     }
 
